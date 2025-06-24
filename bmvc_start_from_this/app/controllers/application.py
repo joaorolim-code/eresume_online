1ce7132d40ec8baf1cfbff9dec3a9e53ba4336ef
João Rolim <joaorolim3008@gmail.com>
2025-06-24 20:06:07 -0300

    Eresume BMVC 2

--- bmvc_start_from_this/app/controllers/application.py
+++ bmvc_start_from_this/app/controllers/application.py
@@ -1,16 +1,22 @@
 from bottle import template
 from bottle import redirect
 from app.controllers.datarecord import DataRecord
+from bottle import request
 
 class Application():
 
     def __init__(self):
+
         self.pages = {
-            'curriculo': self.curriculo
-        }
+        'pagina': self.pagina,
+        'portal': self.portal
+    }
+
+        self.__model= DataRecord()
+        self.__current_loginusername= None
     
-        self.models= DataRecord()
-    
+
+
     def render(self,page,parameter=None):
         content = self.pages.get(page, self.helper)
         if not parameter:
@@ -18,17 +24,46 @@ class Application():
         else:
             return content(parameter)
 
-    def curriculo(self, parameter=None):
-        if not parameter:
-            return template('app/views/html/curriculo', transferred=False) # CORRIGIDO
-        else:
-            info = self.models.work_with_parameter(parameter)
-            if not info:
-                redirect('/curriculo') # CORRIGIDO: use 
-            else:
-                return template('app/views/html/curriculo', transferred=True, data=info) # CORRIGIDO
+
+    def get_session_id(self):
+        return request.get_cookie('session_id')
 
 
     def helper(self):
         return template('app/views/html/helper')
+
+
+    def portal(self):
+        return template('app/views/html/portal')
+
+
+    def pagina(self,username=None):
+        if self.is_authenticated(username):
+            session_id = self.get_session_id()
+            user = self.__model.getCurrentUser(session_id)
+            return template('app/views/html/pagina', current_user=user)
+        else:
+            return template('app/views/html/pagina', current_user=None)
+
+
+    def is_authenticated(self, username):
+        session_id = self.get_session_id()
+        current_username = self.__model.getUserName(session_id)
+        return username == current_username
+
+
+    def authenticate_user(self, username, password):
+        session_id = self.__model.checkUser(username, password)
+        if session_id:
+            self.logout_user()
+            self.__current_username= self.__model.getUserName(session_id)
+            return session_id, username
+        return None
+
+
+    def logout_user(self):
+        self.__current_username= None
+        session_id = self.get_session_id()
+        if session_id:
+            self.__model.logout(session_id)
     
--- bmvc_start_from_this/app/controllers/datarecord.py
+++ bmvc_start_from_this/app/controllers/datarecord.py
@@ -1,25 +1,65 @@
-from app.models.curriculo import Curriculo
+from app.models.curriculo import UserAccount
 import json
+import uuid
 
 class DataRecord():
-    """Banco de dados JSON para o recurso Usuários"""
 
     def __init__(self):
-        self.user_accounts= [] # banco (json)
+
+        self.__user_accounts= []
+        self.__authenticated_users = {}
+        self.read()
+
+
+    def read(self):
         try:
-            with open("app/controllers/db/user_accounts.json", "r") as arquivo_json:
+            with open("app/controllers/db/database.json", "r") as arquivo_json:
                 user_data = json.load(arquivo_json)
-                self.user_accounts = [Curriculo(**data) for data in user_data]
-                self.limit = len(self.user_accounts) - 1  # Definindo o limite com base no número de contas de usuário
+                self.__user_accounts = [UserAccount(**data) for data in user_data]
         except FileNotFoundError:
-            self.user_accounts.append(Curriculo('Guest', '000000'))
-            self.limit = len(self.user_accounts) - 1  # Definindo o limite como 0 no caso de nenhum arquivo encontrado
+            self.__user_accounts.append(UserAccount('Guest', '010101','101010'))
 
 
-    def work_with_parameter(self, parameter):
-        try:
-            index = int(parameter)  # Convertendo o parâmetro para inteiro
-            if index <= self.limit:
-                return self.user_accounts[index]
-        except (ValueError, IndexError):
-            return None  # Tratamento de erro se o índice for inválido
+    def book(self,username,password):
+
+        new_user= UserAccount(username,password)
+        self.__user_accounts.append(new_user)
+        with open("app/controllers/db/user_accounts.json", "w") as arquivo_json:
+            user_data = [vars(user_account) for user_account in \
+            self.__user_accounts]
+            json.dump(user_data, arquivo_json)
+
+
+    def getCurrentUser(self,session_id):
+        if session_id in self.__authenticated_users:
+            return self.__authenticated_users[session_id]
+        else:
+            return None
+
+
+    def getUserName(self,session_id):
+        if session_id in self.__authenticated_users:
+            return self.__authenticated_users[session_id].username
+        else:
+            return None
+
+
+    def getUserSessionId(self, username):
+        for session_id in self.__authenticated_users:
+            if username == self.__authenticated_users[session_id].username:
+                return session_id
+        return None  # Retorna None se o usuário não for encontrado
+
+
+    def checkUser(self, username, password):
+        for user in self.__user_accounts:
+            if user.username == username and user.password == password:
+                session_id = str(uuid.uuid4())  # Gera um ID de sessão único
+                self.__authenticated_users[session_id] = user
+                return session_id  # Retorna o ID de sessão para o usuário
+        return None
+
+
+    def logout(self, session_id):
+        if session_id in self.__authenticated_users:
+            del self.__authenticated_users[session_id] # Remove o usuário logado
